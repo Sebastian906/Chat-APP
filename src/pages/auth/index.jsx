cac46650ac420fc571a9109c195ddc0ca3b81383
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-07-25 20:24:11 -0500

    Configuraciones para los métodos de login y signup, creación de los archivos constants y la api de clientes

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -4,16 +4,74 @@ import Victory from '../../assets/victory.svg';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
+import { apiClient } from '@/lib/api-client';
+import { LOGIN_ROUTE, SIGNUP_ROUTE } from '@/utils/constants';
+import { useNavigate } from 'react-router-dom';
 
 const Auth = () => {
 
+    const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
-    const handleLogin = async () => {};
+    const validateLogin = () => {
+        if (!email.length) {
+            toast.error("Se requiere un correo.");
+            return false;
+        }
+        if (!password.length) {
+            toast.error("Se requiere una contraseña.");
+            return false;
+        }
+        return true;
+    };
 
-    const handleSignup = async () => {};
+    const validateSignup = () => {
+        if (!email.length) {
+            toast.error("Se requiere un correo.");
+            return false;
+        }
+        if (!password.length) {
+            toast.error("Se requiere una contraseña.");
+            return false;
+        }
+        if (password !== confirmPassword) {
+            toast.error("Las contraseñas no coinciden.");
+            return false;
+        }
+        return true;
+    };
+
+    const handleLogin = async () => {
+        if (validateLogin()) {
+            const response = await apiClient.post(
+                LOGIN_ROUTE,
+                {email, password},
+                {withCredentials: true}
+            );
+            if(response.data.user.id) {
+                if(response.data.user.profileSetup) navigate('/chat');
+                else navigate('/profile');
+            }
+            console.log({response});
+        }
+    };
+
+    const handleSignup = async () => {
+        if (validateSignup()) {
+            const response = await apiClient.post(
+                SIGNUP_ROUTE, 
+                {email, password},
+                {withCredentials: true}
+            );
+            if (response.status === 201) {
+                navigate("/profile");
+            }
+            console.log({ response });
+        }
+    };
 
     return (
         <div className='h-[100vh] w-[100vw] flex items-center justify-center'>
@@ -27,7 +85,7 @@ const Auth = () => {
                         <p className='font-medium'>Llena los detalles para empezar con la mejor aplicación de Chat</p>
                     </div>
                     <div className='flex items-center justify-center w-full'>
-                        <Tabs className='w-3/4'>
+                        <Tabs className='w-3/4' defaultValue='login'>
                             <TabsList className='bg-transparent rounded-none w-full'>
                                 <TabsTrigger value='login'
                                 className='data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300'>Identificarse</TabsTrigger>
