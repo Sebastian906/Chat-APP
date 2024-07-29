cc3773ab8174ab6b8b413b65cc7161299b9847c1
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-07-29 13:08:09 -0500

    COnfiguraciones y adiciones de interfaz para el perfil del usuario

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export const colors = [
+  "bg-[#712c4a57] text-[#ff006e] border-[1px] border-[#ff006faa]",
+  "bg-[#ffd60a2a] text-[#ffd60a] border-[1px] border-[#ffd60abb]",
+  "bg-[#06d6a02a] text-[#06d6a0] border-[1px] border-[#06d6a0bb]",
+  "bg-[#4cc9f02a] text-[#4cc9f0] border-[1px] border-[#4cc9f0bb]",
+  "bg-[#7209b72a] text-[#7209b7] border-[1px] border-[#7209b7bb]",
+  "bg-[#3a0ca32a] text-[#3a0ca3] border-[1px] border-[#3a0ca3bb]",
+];
+
+export const getColor = (color) => {
+  if (color >= 0 && color < colors.length) {
+    return colors[color];
+  }
+  return colors[0];
+};
