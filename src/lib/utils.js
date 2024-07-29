d55100c3658c74246b4fe07fd78cf00b3838462e
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-07-29 13:13:15 -0500

    Cambios en el código HEX del color para el avatar

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -10,8 +10,8 @@ export const colors = [
   "bg-[#ffd60a2a] text-[#ffd60a] border-[1px] border-[#ffd60abb]",
   "bg-[#06d6a02a] text-[#06d6a0] border-[1px] border-[#06d6a0bb]",
   "bg-[#4cc9f02a] text-[#4cc9f0] border-[1px] border-[#4cc9f0bb]",
-  "bg-[#7209b72a] text-[#7209b7] border-[1px] border-[#7209b7bb]",
-  "bg-[#3a0ca32a] text-[#3a0ca3] border-[1px] border-[#3a0ca3bb]",
+  "bg-[#7209b72a] text-[#8c0be0] border-[1px] border-[#7209b7bb]",
+  "bg-[#3a0ca32a] text-[#4c11d4] border-[1px] border-[#3a0ca3bb]",
 ];
 
 export const getColor = (color) => {
